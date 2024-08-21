9b6d4855ca63d6ab134c1b28409369868919ce62
Alex Stan <alexandreistan.98@gmail.com>
2024-08-21 15:47:02 +0100

    Added jumpbox

--- security-groups.tf
+++ security-groups.tf
@@ -18,6 +18,30 @@ resource "aws_security_group" "instances" {
   })
 }
 
+resource "aws_security_group" "eks" {
+  name        = "eks_sg"
+  description = "Security group for EKS cluster"
+  vpc_id      = aws_vpc.main.id
+
+  tags = merge(local.tags, {
+    Name = "eks_sg"
+  })
+}
+
+resource "aws_vpc_security_group_egress_rule" "eks_egress_all_ipv4" {
+  security_group_id = aws_security_group.eks.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = -1
+}
+
+resource "aws_vpc_security_group_egress_rule" "eks_egress_all_ipv6" {
+  security_group_id = aws_security_group.eks.id
+
+  cidr_ipv6   = "::/0"
+  ip_protocol = -1
+}
+
 resource "aws_vpc_security_group_egress_rule" "endpoints_egress" {
   security_group_id = aws_security_group.endpoints.id
 
@@ -29,18 +53,14 @@ resource "aws_vpc_security_group_egress_rule" "instances_egress_all_ipv4" {
   security_group_id = aws_security_group.instances.id
 
   cidr_ipv4   = "0.0.0.0/0"
-  ip_protocol = "tcp"
-  from_port   = 443
-  to_port     = 443
+  ip_protocol = -1
 }
 
 resource "aws_vpc_security_group_egress_rule" "instances_egress_all_ipv6" {
   security_group_id = aws_security_group.instances.id
 
   cidr_ipv6   = "::/0"
-  ip_protocol = "tcp"
-  from_port   = 443
-  to_port     = 443
+  ip_protocol = -1
 }
 
 resource "aws_vpc_security_group_ingress_rule" "endpoints_ingress" {
@@ -48,5 +68,14 @@ resource "aws_vpc_security_group_ingress_rule" "endpoints_ingress" {
 
   referenced_security_group_id = aws_security_group.instances.id
   ip_protocol                  = -1
+}
+
+resource "aws_vpc_security_group_ingress_rule" "eks_ingress" {
+  security_group_id = aws_security_group.eks.id
+
+  referenced_security_group_id = aws_security_group.instances.id
+  ip_protocol                  = "tcp"
+  from_port                    = 443
+  to_port                      = 443
 }
 
--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-apt update
-apt install -y awscli 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+apt-get update
+apt-get install -y unzip apt-transport-https ca-certificates curl gnupg
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+chmod 644 /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubectl
+
